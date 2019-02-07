a2722e1714740822411db1db6964156f77f530a3
Debora <debora.piu92@gmail.com>
2019-02-07 14:31:21 +0000

    increase and decrease temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,4 +9,14 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(20);
   });
 
+  it('can increase the temperature', function() {
+    thermostat.increase(5);
+    expect(thermostat.temperature).toEqual(25);
+  });
+
+  it('can decrease the temperature', function() {
+    thermostat.decrease(5);
+    expect(thermostat.temperature).toEqual(15);
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temperature = 20;
 }
+
+Thermostat.prototype.increase = function(num) {
+  this.temperature += num;
+}
+
+Thermostat.prototype.decrease = function(num) {
+  this.temperature -= num;
+}
